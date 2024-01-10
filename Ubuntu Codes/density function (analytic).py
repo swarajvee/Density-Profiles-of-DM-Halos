e50bc475b4c82d16439eedcec8f4b7b79ae4925c
swaraj <000swaraj000@gmail.com>
2024-01-10 21:05:00 +0530

    imported required python files into ubuntu folder

--- Ubuntu Codes/density function (analytic).py
+++ Ubuntu Codes/density function (analytic).py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt 
@@ -9,7 +8,7 @@ cosmology.setCosmology('planck18')
 fig=plt.figure(figsize=(10,8),dpi=100)
 
 #analytic method
-def mass_density_fun(file_path,l,b):
+def mass_density_fun(file_path,l,b,z_rd):
     h = 0.6736
     box_size = l/h
     V= box_size**3
@@ -23,7 +22,7 @@ def mass_density_fun(file_path,l,b):
     z = data['Z'].tolist()
     bineq = np.logspace(np.log10(min(N)),np.log10(max(N)),b)
 
-    frequency, bin_edge = np.histogram(np.log10(N),bins=bineq)    
+    frequency, bin_edge = np.histogram(N,bins=bineq)    
     mass_pt = []
     bin_width = []
     for k in range(len(bin_edge)-1):
@@ -35,36 +34,20 @@ def mass_density_fun(file_path,l,b):
     for q in range(len(mass_pt)):
         density_fun.append(frequency[q]/(bin_width[q]*V))
     plt.loglog()
+    plt.title(f'Mass Density vs Mass (Redshift: {z_rd})')
+    plt.xlabel(r'$\log_{10} (\mathrm{Mass})$')
+    plt.ylabel(r'$\log_{10} (\mathrm{Density Function})$')
+    
     plt.scatter(mass_pt,density_fun,label='Observed Mass Density',s=7)
     
-#from colossus
-def MassFunc(z,M,l):
-    h = 0.6736
-    box_size = l/h
-    V= box_size**3
+    plt.legend()
+    plt.grid()
+    plt.show()
+    
+    
 
-    plt.loglog()
-    mfunc = mass_function.massFunction(M, z, mdef = '200m', model = 'tinker08', q_out ='dndlnM')
-    plt.plot(M, mfunc/(M),'--', label = 'Mass Density using Colossus Package', alpha=0.2, color='red')
-
-
-#for analytic method
 file_path = '/home/swaraj/Documents/Hubble data/csv data/z8.csv'
 l = 500 # box size
 b= 20 #no of bins
-mass_density_fun(file_path,l,b)
-
-
-
-
-#for colossus 
-z = 8 #redshift
-M = 10**np.arange(11.0, 12.3, 0.1)
-MassFunc(z,M,l)
-
-plt.title(f'Mass Density vs Mass (Redshift: {z})')
-plt.xlabel(r'$\log_{10} (\mathrm{Mass})$')
-plt.ylabel(r'$\log_{10} (\mathrm{Density Function})$')
-plt.legend()
-plt.grid()
-plt.show()
+z_rd= 8
+mass_density_fun(file_path,l,b,z_rd)

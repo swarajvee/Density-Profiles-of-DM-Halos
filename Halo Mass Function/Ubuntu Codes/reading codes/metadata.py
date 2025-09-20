5640f729d2dd325a421d882a6d8416feef91f9ba
swaraj <106585707+swarajvee@users.noreply.github.com>
2025-09-20 13:48:50 +0530

    Delete Halo Mass Function directory

--- Halo Mass Function/Ubuntu Codes/reading codes/metadata.py
+++
@@ -1,5 +0,0 @@
-import abacusnbody.metadata
-
-#meta = abacusnbody.metadata.get_meta('simname':'AbacusSummit_base_ph000_c000', 'redshift':2)
-meta = abacusnbody.metadata.get_meta('AbacusSummit_base_c000_ph000', redshift=8)
-print(meta['Om0'])
--- Halo Mass Function/mass density function per unit volume/density function/colossus/setting an new cosmology.py
+++
@@ -1,7 +0,0 @@
-from colossus.cosmology import cosmology
-
-#adding a new cosmology
-params = {'flat': True, 'H0': 67.36, 'Om0': 0.31, 'Ob0': 0.049, 'sigma8': 0.81, 'ns': 0.95}
-cosmology.addCosmology('myCosmo', **params)
-cosmo = cosmology.setCosmology('myCosmo')
-
